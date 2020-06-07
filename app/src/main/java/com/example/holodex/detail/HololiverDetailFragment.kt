7f113766799f7d2af96e4728ff5da2b2f5d92ca8
Youhei Oonuma <oonuma617@gmail.com>
2020-06-07 22:01:15 +0900

    stream info

--- app/src/main/java/com/example/holodex/detail/HololiverDetailFragment.kt
+++ app/src/main/java/com/example/holodex/detail/HololiverDetailFragment.kt
@@ -5,15 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.holodex.R
+import com.example.holodex.databinding.FragmentHololiverDetailBinding
 
 class HololiverDetailFragment : Fragment() {
+
+    lateinit var binding: FragmentHololiverDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_hololiver_detail, container, false)
+
+        binding = FragmentHololiverDetailBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = StreamInfoAdapter(requireContext())
+        binding.streamInfoDetail.adapter = adapter
+
+        val list = (1..3).map {
+            StreamItem(
+                "title",
+                "description",
+                "url"
+            )
+        }
+
+        adapter.submitList(list)
     }
 }
---
+++ app/src/main/java/com/example/holodex/detail/StreamItem.kt
@@ -0,0 +1,7 @@
+package com.example.holodex.detail
+
+data class StreamItem(
+    val title: String,
+    val description: String,
+    val thumbnail: String
+)
