5302e9932716690622212e952face6ec6c44e8f2
Youhei Oonuma <oonuma617@gmail.com>
2020-06-13 13:17:40 +0900

    small changes

--- app/src/test/java/com/example/holodex/ExampleUnitTest.kt
+++ app/src/test/java/com/example/holodex/ExampleUnitTest.kt
@@ -1,21 +1,5 @@
 package com.example.holodex
 
-import com.example.holodex.di.AppModule
-import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.runBlocking
-
-import org.junit.Test
-
 class ExampleUnitTest {
 
-    val hololiveAPI = AppModule.provideHoloLiverAPI()
-
-    @Test
-    fun ときのそら_取得できる() {
-        runBlocking {
-            val members = hololiveAPI.hololiveMembers()
-            assertThat(members.first().name)
-                .isEqualTo("ときのそら")
-        }
-    }
 }
