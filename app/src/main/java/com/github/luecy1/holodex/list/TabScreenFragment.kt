29ccb5a1f690b767d2a329919b0d25c266d7a755
Youhei Oonuma <oonuma617@gmail.com>
2021-12-05 11:26:28 +0900

    error?

---
+++ app/src/main/java/com/github/luecy1/holodex/list/TabScreenFragment.kt
@@ -0,0 +1,15 @@
+package com.github.luecy1.holodex.list
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.github.luecy1.holodex.R
+
+class TabScreenFragment : Fragment(R.layout.fragment_tab_screen) {
+
+//    lateinit var binding:Tab
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
