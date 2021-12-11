4b762768294e841579be35f622492c5d88e25cc6
Youhei Oonuma <oonuma617@gmail.com>
2021-12-12 00:14:12 +0900

    compose setup

--- app/src/main/java/com/github/luecy1/holodex/MainActivity.kt
+++ app/src/main/java/com/github/luecy1/holodex/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.github.luecy1.holodex
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setContent {
+
+        }
     }
 }
