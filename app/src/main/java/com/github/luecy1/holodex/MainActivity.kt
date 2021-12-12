7414475b103480a6681bf0d16821607f1849eaba
Youhei Oonuma <oonuma617@gmail.com>
2021-12-12 18:59:00 +0900

    item

--- app/src/main/java/com/github/luecy1/holodex/MainActivity.kt
+++ app/src/main/java/com/github/luecy1/holodex/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.github.luecy1.holodex
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -53,7 +54,7 @@ fun LiverItem() {
         Text(
             text = "雪花ラミィ",
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.h6,
+            style = MaterialTheme.typography.h6.copy(color = MaterialTheme.colors.textColor),
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(
@@ -62,13 +63,53 @@ fun LiverItem() {
                         bottomEnd = roundedCornerSize
                     )
                 )
-                .background(Color.LightGray)
+                .background(MaterialTheme.colors.textBackgroundColor)
         )
     }
 }
 
+val Colors.textColor: Color
+    get() = if (isLight) Color.Black else Color.White
+
+val Colors.textBackgroundColor: Color
+    get() = if (isLight) Color.LightGray else Color(0xea, 0x80, 0xfc)
+
+
 @Preview(showBackground = false)
 @Composable
 fun hoge() {
-    LiverItem()
+    HoloDexTheme {
+        LiverItem()
+    }
+}
+
+@Preview(showBackground = false, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun hoge2() {
+    HoloDexTheme(darkTheme = true) {
+        LiverItem()
+    }
+}
+
+@Composable
+fun HoloDexTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        darkColors()
+    } else {
+        lightColors()
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography(
+            h6 = MaterialTheme.typography.h6.copy(
+                color = MaterialTheme.colors.textColor
+            )
+        ),
+        shapes = Shapes(),
+        content = content
+    )
 }
